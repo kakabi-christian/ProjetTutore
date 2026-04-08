b8b7ded23593e61c59528e9e53bef9c865e474d5
Kakabi Christian <kakabichristian@gmail.com>
2026-04-08 11:02:31 +0100

    mise en place du clean code

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -6,30 +6,23 @@ return [
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
     */
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    'allowed_methods' => ['*', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
     'allowed_origins' => [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'https://5982-102-244-223-207.ngrok-free.app', // Ton tunnel ngrok
+        'https://5982-102-244-223-207.ngrok-free.app',
     ],
 
     'allowed_origins_patterns' => [
         '#^https://.*\.ngrok-free\.app$#',
     ],
 
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
